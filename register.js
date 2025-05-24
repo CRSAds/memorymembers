a03cf7f1b07aa0929280caa116b9ce12c3a9b34d
CRSAds <yuri@crsadvertising.com>
2025-05-24 12:38:38 +0200

    Update register.js

--- register.js
+++ register.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (response.ok) {
         alert("Account succesvol aangemaakt!");
-        window.location.href = "/memorymembers/login"; // pas aan naar jouw loginpagina
+        window.location.href = `${window.location.origin}/memorygamespelen`;
       } else {
         console.error(result);
         alert("Fout bij registreren: " + (result.errors?.[0]?.message || result.message || "Onbekende fout"));
